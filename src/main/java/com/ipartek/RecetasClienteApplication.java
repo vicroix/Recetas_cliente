0fe4035a14012417b8e87b026e07364b2ff4159d
vicroix <victor.batista.jaldon@gmail.com>
2026-03-25 10:52:01 +0100

    Initial commit

---
+++ src/main/java/com/ipartek/RecetasClienteApplication.java
@@ -0,0 +1,13 @@
+package com.ipartek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecetasClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecetasClienteApplication.class, args);
+	}
+
+}
